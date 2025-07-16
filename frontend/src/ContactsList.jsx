30cfc1eb9d980aec4f463ccc2fc5a3a839d179ea
Peter Chay <peterchay88@gmail.com>
2025-07-15 23:28:57 -0600

    initial layout of the page

--- frontend/src/ContactsList.jsx
+++ frontend/src/ContactsList.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 /**
- * 
+ * Creates the layout of the page 
  * @param {*} param0 
  */
 const ContactList = ({contacts}) => {
+    return <div>
+        <h2>Contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) =>(
+                    <tr></tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
 
 }
